fedce0d21eb669a713a19859703fd0b913adc47a
elaheabdali <ohelahe@gmail.com>
2023-10-19 23:47:19 +0330

    change sidebar

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -63,7 +63,6 @@ const Sidebar = () => {
     >
       <ProSidebar collapsed={isCollapsed}>
         <Menu iconShape='square'>
-          {/* LOGO AND MENU ICON */}
           <MenuItem
             onClick={() => setIsCollapsed(!isCollapsed)}
             icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
@@ -96,7 +95,7 @@ const Sidebar = () => {
                   alt='profile-user'
                   width='100px'
                   height='100px'
-                  src={`../../assets/user.png`}
+                  src={`../../assets/user.jpg`}
                   style={{ cursor: 'pointer', borderRadius: '50%' }}
                 />
               </Box>
@@ -107,7 +106,7 @@ const Sidebar = () => {
                   fontWeight='bold'
                   sx={{ m: '10px 0 0 0' }}
                 >
-                  Ed Roh
+                  Elahe
                 </Typography>
                 <Typography variant='h5' color={colors.greenAccent[500]}>
                   VP Fancy Admin
