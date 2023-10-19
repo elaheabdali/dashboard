6a42f5afd9bbae3e0693fedf0123e65b0c076d50
elaheabdali <ohelahe@gmail.com>
2023-10-20 01:03:39 +0330

    change index

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Header from '../../components/Header';
+import { Box } from '@mui/material';
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <Box m='20px'>
+      <Box display='flex' justifyContent='space-between' alignItems='center'>
+        <Header title='DASHBOARD' subtitle='Welcome to your dashboard' />
+      </Box>
+    </Box>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
