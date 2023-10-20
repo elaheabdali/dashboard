60fe9852cb19205bcf8969a3539807aa51967622
elaheabdali <ohelahe@gmail.com>
2023-10-21 00:13:17 +0330

    change

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import Sidebar from './scenes/global/Sidebar';
 import Dashboard from './scenes/dashboard';
 import Contacts from './scenes/contacts';
 import Team from './scenes/team';
-/*
 import Invoices from './scenes/invoices';
-import Bar from './scenes/bar';
 import Form from './scenes/form';
-import Line from './scenes/line';
+import Bar from './scenes/bar';
 import Pie from './scenes/pie';
-import FAQ from './scenes/faq';
+import Line from './scenes/line';
 import Geography from './scenes/geography';
+import FAQ from './scenes/faq';
 import Calendar from './scenes/calendar/calendar';
-*/
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { ColorModeContext, useMode } from './theme';
 import { Routes, Route } from 'react-router-dom';
@@ -34,6 +32,14 @@ function App() {
               <Route path='/' element={<Dashboard />} />
               <Route path='/team' element={<Team />} />
               <Route path='/contacts' element={<Contacts />} />
+              <Route path='/invoices' element={<Invoices />} />
+              <Route path='/form' element={<Form />} />
+              <Route path='/calendar' element={<Calendar />} />
+              <Route path='/faq' element={<FAQ />} />
+              <Route path='/bar' element={<Bar />} />
+              <Route path='/pie' element={<Pie />} />
+              <Route path='/line' element={<Line />} />
+              <Route path='/geography' element={<Geography />} />
             </Routes>
           </main>
         </div>
@@ -43,16 +49,3 @@ function App() {
 }
 
 export default App;
-/*
-
-              
-              
-              <Route path='/invoices' element={<Invoices />} />
-              <Route path='/form' element={<Form />} />
-              <Route path='/bar' element={<Bar />} />
-              <Route path='/pie' element={<Pie />} />
-              <Route path='/line' element={<Line />} />
-              <Route path='/faq' element={<FAQ />} />
-              <Route path='/calendar' element={<Calendar />} />
-              <Route path='/geography' element={<Geography />} />
-              */
